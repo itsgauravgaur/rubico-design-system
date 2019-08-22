b928bc69c0c03634bee4e397e37c2960cac1a584
Evan Gaurav <evan.gaurav@ithands.net>
2019-08-22 09:57:01 +0530

    1. change styl to scss 2. import jquery

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.styl']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showInput(){
+    // $(".srch").toggle(500,"swing");
+    $(".srch").animate({
+      width: "toggle"
+    });
   }
 
 }
