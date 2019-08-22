e2c1229940e19206db44d8a7a286765b1f222c89
Mohit <mohit@Mohit.local>
2019-08-22 11:20:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { BannerComponent } from './content-area/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    ContentAreaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
