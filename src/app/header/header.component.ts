8802cc453aeda803f9cf941d77e248115956351c
Evan Gaurav <evan.gaurav@ithands.net>
2019-08-29 14:34:08 +0530

    done sidebar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,63 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+selector: 'app-header',
+templateUrl: './header.component.html',
+styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  screenWidth:number = 0;
 
-  ngOnInit() {
+constructor() { }
+
+ngOnInit() {
+  this.checksize();
+}
+
+
+
+
+  checksize = () => {
+    $(window).on('resize load', function() { 
+      this.screenWidth = $(window).width();
+      if(this.screenWidth < 768) {
+        hideSidebar();
+      }else {
+        showSidebar();
+      }
+    });
   }
+
+
+  toggleSidebar() {
+    if($(".sidebar").css('left') == '0px'){
+      hideSidebar();
+    }else {    
+      showSidebar();
+      
+    }
+  }  
+}
+
+
+function showSidebar() {
+    $(".content").css({
+      "margin-left": "261px"
+    })
+    setTimeout(()=> {
+      $(".sidebar").css({
+        "left": "0px"
+      });
+    }, 250)
+ }
+
+
+ function hideSidebar() {
+  $(".sidebar").css({
+    "left": "-260px"
+    });
+    $(".content").css({
+      "margin-left": "0px"
+    })
 }
