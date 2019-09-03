72657b184786bc1281837ae56ab95203b9cec804
Mohit <mohit@Mohit.local>
2019-09-03 12:08:15 +0530

    app router for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './content-area/about-us/about-us.component';
+import { HomeComponent } from './content-area/home/home.component';
 
 const routes: Routes = [
-  { path: 'about-us', component: AboutUsComponent }
+  { path: 'about-us', component: AboutUsComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
