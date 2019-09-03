4a28bbaea1e805132e9c1143bf32e039ef3071cd
Evan Gaurav <evan.gaurav@ithands.net>
2019-09-03 10:35:45 +0530

    add router liink

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './content-area/about-us/about-us.component';
+
+const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentAreaComponent } from './content-area/content-area.component';
 import { BannerComponent } from './content-area/banner/banner.component';
+import { AboutUsComponent } from './content-area/about-us/about-us.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './content-area/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +17,13 @@ import { BannerComponent } from './content-area/banner/banner.component';
     HeaderComponent,
     SidebarComponent,
     ContentAreaComponent,
-    BannerComponent
+    BannerComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
